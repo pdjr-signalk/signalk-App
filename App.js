486fc9ff10fe7b357f6b2db11f607e0e555a3abf
Paul Reeve <preeve@pdjr.eu>
2023-10-19 13:49:38 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,36 @@
+"use strict;"
+
+/**********************************************************************
+ * Copyright 2022 Paul Reeve <preeve@pdjr.eu>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+const Delta = require('../signalk-libdelta/Delta.js');
+const Notification = require('../signalk-libnotification/Notification.js');
+
+module.exports = class App {
+
+  constructor(app, id) {
+    this.app = app;
+    this.delta = new Delta(app, id);
+  }
+
+  notify(path, value, sourceId) {
+    var notificationPath = (path.startsWith("notifications."))?path:("notifications." + path);
+    if (value !== null) value = Notification.makeNotification(path, value);
+    this.delta.clear().addValue(notificationPath, value).commit().clear();
+    return((value)?value.id:null);
+  }
+  
+}
